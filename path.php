f4d45995848be81419dba06334497e165a3fb719
ErSebah <elyordiusc@gmail.com>
2020-04-26 17:57:36 -0500

    Agregado Directorio de rutas

---
+++ path.php
@@ -0,0 +1,9 @@
+<?php 
+
+define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT'].'/mindblow/');
+define('CONTROLLER_PATH', ROOT_PATH.'controller/');
+define('MODEL_PATH', ROOT_PATH.'model/');
+define('VIEW_PATH', ROOT_PATH.'view/');
+define('ASSETS_PATH', ROOT_PATH.'assets/');
+
+?>
